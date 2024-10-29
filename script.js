32e502e5c65d0246de5699f4b1c0b44453f84d2c
hjhkhvffsd <matteosicard8@gmail.com>
2024-10-29 19:44:28 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function navigate() {
-    var url = document.getElementById('url').value;
-    if (!url.startsWith('http://') && !url.startsWith('https://')) {
-        url = 'http://' + url;
-    }
-    document.getElementById('browser-frame').src = url;
-}
